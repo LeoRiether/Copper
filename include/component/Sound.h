e0a1e7da19b8be9c2c763e3f05bb795f5814b289
LeoRiether <leonardoalves8217@gmail.com>
2023-10-20 20:17:28 -0300

    cleanup: delete unused classes

---
+++ include/component/Sound.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "CType.h"
+#include "Component.h"
+#include "GameObject.h"
+#include "SDL_include.h"
+#include "util.h"
+#include "wrap/SoundChunk.h"
+
+using std::shared_ptr;
+using std::string;
+
+class Sound : public Component {
+   private:
+    shared_ptr<SoundChunk> chunk;
+    int channel;
+
+   public:
+    Sound(GameObject& associated);
+    Sound(GameObject& associated, const string& file);
+    ~Sound();
+
+    void Play(int times = 1);
+    void Stop();
+    void Open(const string& file);
+    bool IsOpen();
+    bool IsPlaying();
+
+    void Update(float dt);
+    void Render(Vec2<Cart> camera);
+    bool Is(CType type);
+};
