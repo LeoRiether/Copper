83029a7bb4a3e9c078962f91df075960fe19bfb8
LeoRiether <leonardoalves8217@gmail.com>
2023-11-01 14:57:11 -0300

    Revert "Fix coords system"
    This reverts commit 5622ea4d4cb24e315b798dd8baef59dc045f7c40.

--- src/math/Vec2.cpp
+++ src/math/Vec2.cpp
@@ -1,7 +1,5 @@
 #include "math/Vec2.h"
 
-constexpr double SIN_PI_3 = 0.8660254037844386;
-
 template <>
 Vec2<Iso> Vec2<Iso>::toIso() const {
     return {x, y};
@@ -9,12 +7,12 @@ Vec2<Iso> Vec2<Iso>::toIso() const {
 
 template <>
 Vec2<Iso> Vec2<Cart>::toIso() const {
-    return {float(0.5 / SIN_PI_3 * x + y), float(-0.5 / SIN_PI_3 * x + y)};
+    return {x + 2.0f * y, -x + 2.0f * y};
 }
 
 template <>
 Vec2<Cart> Vec2<Iso>::toCart() const {
-    return {float(SIN_PI_3 * (x - y)), 0.5f * (x + y)};
+    return {(x - y) / 2.0f, (x + y) / 4.0f};
 }
 
 template <>
